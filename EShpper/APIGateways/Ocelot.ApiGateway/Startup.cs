6ece9ce235e637166dd37e0bdbb803bce8874e3c
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-10-14 16:39:10 +0100

    completed API Gateway with Ocelot

---
+++ EShpper/APIGateways/Ocelot.ApiGateway/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.ApiGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot()
+                .AddCacheManager(O => O.WithDictionaryHandle());
+        }
+
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Ocelot");
+                });
+            });
+
+            await app.UseOcelot();
+        }
+    }
+}
